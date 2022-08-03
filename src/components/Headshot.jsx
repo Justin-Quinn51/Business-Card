60408cfa656810441e8356054254b4e3e88b8dad
Justin Quinn <jpq3366@gmail.com>
2022-08-02 20:56:30 -0400

    Centered image with flex

--- src/components/Headshot.jsx
+++ src/components/Headshot.jsx
@@ -4,7 +4,7 @@ import React from "react"
 
 function Headshot() {
     return (
-        <div>
+        <div className='flex justify-center'>
             <img className="h-[25rem] w-[25rem]" src="/Rectangle90.png" alt="profile-picture"/>
         </div>
     )
